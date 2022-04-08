084f6df540fab03f994a7a4804166cf30fdeeb87
Max Fonss <mjfonss@email.wm.edu>
2022-04-08 17:06:40 -0400

    ready for first step testing

--- StudentSocketImpl-skel.java
+++ StudentSocketImpl-skel.java
@@ -39,10 +39,10 @@ class StudentSocketImpl extends BaseSocketImpl {
     localport = D.getNextAvailablePort();
     ackNum = 0;
     seqNum = 0;
-    curState = CLOSED;
+    curState = TCPState.CLOSED;
     D.registerConnection(remoteAddress, localport, remotePort, this);
     sendAndWrapPacket(remoteAddress, remotePort, false, true, false, windowSize, data);
-
+    change_state(curState, TCPState.SYN_SENT);
   }
   
   /**
@@ -50,7 +50,7 @@ class StudentSocketImpl extends BaseSocketImpl {
    * @param p The packet that arrived
    */
   public synchronized void receivePacket(TCPPacket p){
-    system.out.println(p.toString());
+    System.out.println(p.toString());
   }
   
   /** 
@@ -61,6 +61,9 @@ class StudentSocketImpl extends BaseSocketImpl {
    * Note that localport is already set prior to this being called.
    */
   public synchronized void acceptConnection() throws IOException {
+    curState = TCPState.CLOSED;
+    D.registerListeningSocket(localport, this);
+    change_state(curState, TCPState.LISTENING);
   }
 
   
@@ -133,5 +136,10 @@ class StudentSocketImpl extends BaseSocketImpl {
     TCPPacket packetToSend = new TCPPacket(localport, remotePort, ackNum, seqNum, ackFlag, synFlag, finFlag, windowSize, data);
     TCPWrapper.send(packetToSend, remoteAddress);
   }
+
+  public void change_state(TCPState curState, TCPState state_change_to){
+    this.curState = state_change_to;
+    System.out.print("\n ACTION-STATE-CHANGED: " + curState + " to " + state_change_to + "\n");
+}
 
 }
